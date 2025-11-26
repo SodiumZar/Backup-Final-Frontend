4f984547b63a2e04ad53b3a93048157611ca75d4
Noel <imanuelpalenewen15@gmail.com>
2025-11-26 10:58:50 +0800

    feat(api): configure axios base instance with interceptors

--- src/RestAPI.jsx
+++ src/RestAPI.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import api from "./services/api";
 
 const RestAPI = () => {
+    console.log('API instance created:', api.defaults.baseURL)
     return (
         <>
-
+            <h1> testing API</h1>
         </>
     );
 };
--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,49 @@
 // Base API configuration
+import axios from "axios"
+
+const api_url = "http://localhost:3000"
+
+const api = axios.create({
+  baseURL : api_url, 
+  headers: {
+    "Content-Type" : "application/json"
+  },
+  timeout: 10000, // 10 dtik
+})
+
+
+// Request interceptor
+
+// Logging atau auth
+api.interceptors.request.use (
+  (config) => {
+    console.log(`[API Request] ${config.method.toUpperCase()} ${config.url}`)
+  
+    return config
+  },
+  (error) => {
+    console.log(`API Requested Error : ${error}`)
+    return Promise.reject (error)
+  }
+) 
+
+// Error Handling
+
+api.interceptors.response.use(
+  (response) => {
+    console.log(`[API Response] ${response.status} ${response.config.url}`)
+    return response
+  },
+  (error) => {
+    if (error.response) {
+      console.error('[API Response Error]',  error.response.status, error.response.data)
+  } else if (error.request) {
+    console.error('[API No Response]', error.request)
+  } else {
+    console.error('[API Error]'), error.message
+  }
+  return Promise.reject(error)
+  }
+)
+
+export default api;
